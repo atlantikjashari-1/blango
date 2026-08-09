0f98b6835c59c0a6661ef11fe7c97943e3ced7c8
Atlantik Jashari <atlantik@anchorzup.com>
2026-08-09 01:45:09 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -36,7 +36,7 @@ CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
 
 
-# Application definition
+# Application nition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
