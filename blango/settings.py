4567669f3506288162dd1795aaa25f61383c8e17
Atlantik Jashari <atlantik@anchorzup.com>
2026-08-24 11:57:16 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+AUTH_USER_MODEL = "blango_auth.User"
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-'
 
@@ -45,9 +47,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog",
-    "crispy_forms",
-    "crispy_bootstrap5",
+    'blango_auth',
+    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 LOGGING = {
